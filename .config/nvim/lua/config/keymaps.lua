bd2160298d230dff34d7ee3f7ab55344448b7d21
rudrah <>
2026-09-12 10:21:27 +0530

    Add keymap for tab

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,13 +1,23 @@
 vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil" })
 vim.keymap.set("n", "gl", function()
-    vim.diagnostic.open_float()
+	vim.diagnostic.open_float()
 end, { desc = "Open Diagnostics in Float" })
 
 vim.keymap.set("n", "<leader>cf", function()
-    require("conform").format({
-        lsp_format = "fallback",
-    })
+	require("conform").format({
+		lsp_format = "fallback",
+	})
 end, { desc = "Format current file" })
 
 -- Map <leader>fp to open projects
 vim.keymap.set("n", "<leader>fp", ":ProjectFzf<CR>", { noremap = true, silent = true })
+
+-- Create a new tab
+vim.keymap.set("n", "<leader>tn", ":tabnew<CR>", { desc = "New tab" })
+
+-- Navigate tabs quickly
+vim.keymap.set("n", "<A-.>", ":tabnext<CR>", { desc = "Next tab" })
+vim.keymap.set("n", "<A-,>", ":tabprevious<CR>", { desc = "Previous tab" })
+
+-- Close tab
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { desc = "Close tab" })
